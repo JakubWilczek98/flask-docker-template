f849a37df735a091e66d0a1399de54f897b53e16
JakubWilczek98 <JakubWilczek98@github.com>
2022-10-31 09:49:37 +0100

    Flask on docker with hot reload

--- web/app.py
+++ web/app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hellow_world():
-    return 'Flask Dockerized'
+def hello_world():
+    return 'Hello world'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
